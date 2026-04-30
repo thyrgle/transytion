ed2a27f9d6210e0dc17086eb89b1266b23da7781
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-04-29 17:26:56 -0700

    Version bump

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'transytion'
 copyright = '2026, Christopher Sumnicht'
 author = 'Christopher Sumnicht'
-release = '0.1.0'
+release = '0.2.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
