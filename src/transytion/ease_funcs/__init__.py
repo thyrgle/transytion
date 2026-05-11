f2d9fcbae100b974f43668ca671afaf2f9dc7ffb
Christopher Sumnicht <csumnicht@berkeley.edu>
2026-05-11 16:38:50 -0700

    Added more ease funcs.

--- src/transytion/ease_funcs/__init__.py
+++ src/transytion/ease_funcs/__init__.py
@@ -1,7 +1,12 @@
 # Based on https://github.com/rxi/flux/blob/master/flux.lua
+# and https://easings.net/
 from collections.abc import Callable
 import math
 
+
+def sine(x: float) -> float:
+    """Sinusoidal tween."""
+    return 1 - math.cos(x * math.pi / 2)
 
 def linear(x: float) -> float:
     """Linear tween that returns the thing itself."""
@@ -23,10 +28,48 @@ def quint(x: float) -> float:
     """Quintic tween, returns itself raised to the fifth power."""
     return x ** 5
 
-def sine(x: float) -> float:
-    """Sinusoidal tween."""
-    return 1 - math.cos(x * math.pi / 2)
+def expo(x: float) -> float:
+    """Exponential tween, returns 2^(10x - 10)."""
+    if x == 0:
+        return 0
+    return 2 ** (10 * x - 10)
 
+def circ(x: float) -> float:
+    """Returns a circular tween. See https://easings.net/#easeInCirc"""
+    return 1 - math.sqrt(1 - x ** 2)
+
+def back(x: float) -> float:
+    """Returns 'back' tween. See: https://easings.net/#easeInBack"""
+    c1 = 1.70158
+    c2 = c1 + 1
+    return c2 * (x ** 3) - c1 * (x ** 2)
+
+def elastic(x: float) -> float:
+    """Returns elastic tween. See https://easings.net/#easeInElastic"""
+    c = (2 * math.pi) / 3
+    if x == 0:
+        return 0
+    elif x == 1:
+        return 1
+    return -(2 ** (10 * x - 10)) * math.sin((x * 10 - 10.75) * c)
+
+def _bounce(x: float) -> float:
+    """Performs bound (out) tween. See https://easings.net/#easeOutBounce"""
+    n = 7.5625
+    d = 2.75
+
+    if x < 1 / d:
+        return n * x ** 2
+    elif x < 2 / d:
+        x -= 1.5
+        return n * (x / d) * x + 0.75
+    elif x < 2.5 / d:
+        x -= 2.625
+        return n * (x / d) * x + 0.984375
+
+def bounce(x: float) -> float:
+    """Performs bounce (in) tween. See https://easings.net/#easeInBounce"""
+    return 1 - _bounce(x)
 
 # Common in-out functions (in is just the original function).
 # See https://hump.readthedocs.io/en/latest/timer.html#tweening-methods for
